cd0ae840eed0f55c81d412b6e54a49620852ef7a
kenneth palermo <kennethross@aspevo.com>
2020-11-13 10:08:11 +0800

    first push

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -11,20 +11,21 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  let { title, description, imageUrl, price } = req.body;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('admin/product-list', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
       hasProducts: products.length > 0,
       activeShop: true,
       productCSS: true
     });
-  });
+  })
 };
